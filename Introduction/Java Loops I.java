d5427b952f2a174c7e5f72cccaf6acacaa70a693
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-03-05 04:56:44 -0300

    Update Java Loops I.java

--- Introduction/Java Loops I.java
+++ Introduction/Java Loops I.java
@@ -1,12 +1,23 @@
 // https://www.hackerrank.com/challenges/java-loops-i/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
 
-        multiplicationTable(2);
+        multiplicationTable(N);
+
 
         bufferedReader.close();
     }
