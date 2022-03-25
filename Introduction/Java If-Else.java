ba351a8d704a18a19c9bf32dea21ffb0196129ac
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-03-25 13:14:09 -0300

    Create Java If-Else.java

---
+++ Introduction/Java If-Else.java
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/java-if-else
+
+import java.util.Scanner;
+
+class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        } else {
+            if (N > 2 && N < 5) {
+                System.out.println("Not Weird");
+            } else if (N > 6 && N < 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+
+        scanner.close();
+    }
+}
