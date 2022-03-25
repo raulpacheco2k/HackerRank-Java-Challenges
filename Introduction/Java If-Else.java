acbfa6a071d7906d075b2c6c2a222422f06fb35a
Raul Pacheco <61039190+raulpacheco2k@users.noreply.github.com>
2022-03-25 13:18:22 -0300

    Update Java If-Else.java

--- Introduction/Java If-Else.java
+++ Introduction/Java If-Else.java
@@ -12,9 +12,9 @@ class Solution {
         if (N % 2 == 1) {
             System.out.println("Weird");
         } else {
-            if (N > 2 && N < 5) {
+            if (N >= 2 && N <= 5) {
                 System.out.println("Not Weird");
-            } else if (N > 6 && N < 20) {
+            } else if (N >= 6 && N <= 20) {
                 System.out.println("Weird");
             } else {
                 System.out.println("Not Weird");
